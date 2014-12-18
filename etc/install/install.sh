0a04c44da9895d2ad601bea9855fe0913d22ccfd
Ben Tappin <ben@mrben.co.uk>
2014-12-18 19:04:34 +0000

    Update install to work with Debian and Code Kitchen practices.

--- etc/install/install.sh
+++ etc/install/install.sh
@@ -11,7 +11,6 @@ VIRTUALENV_NAME=$PROJECT_NAME
 
 PROJECT_DIR=/home/vagrant/$PROJECT_NAME
 VIRTUALENV_DIR=/home/vagrant/.virtualenvs/$PROJECT_NAME
-LOCAL_SETTINGS_PATH="/$PROJECT_NAME/settings/local.py"
 
 PGSQL_VERSION=9.3
 
@@ -25,11 +24,14 @@ export LANG=en_GB.UTF-8
 export LC_ALL=en_GB.UTF-8
 
 # Install essential packages from Apt
-apt-get update -y
+apt-get update
+apt-get upgrade -y
 # Python dev packages
 apt-get install -y build-essential python python-dev
 # python-setuptools being installed manually
-wget https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py -O - | python
+curl --silent --show-error --retry 5 https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py | python
+# Install pip
+curl --silent --show-error --retry 5 https://raw.githubusercontent.com/pypa/pip/master/contrib/get-pip.py | python
 # Dependencies for image processing with Pillow (drop-in replacement for PIL)
 # supporting: jpeg, tiff, png, freetype, littlecms
 # (pip install pillow to get pillow itself, it is not in requirements.txt)
@@ -38,16 +40,15 @@ apt-get install -y libjpeg-dev libtiff-dev zlib1g-dev libfreetype6-dev liblcms2-
 apt-get install -y git
 
 # Postgresql
-if ! command -v psql; then
-    apt-get install -y postgresql-$PGSQL_VERSION libpq-dev
-    cp $PROJECT_DIR/etc/install/pg_hba.conf /etc/postgresql/$PGSQL_VERSION/main/
-    /etc/init.d/postgresql reload
-fi
+grep -q -F "deb http://apt.postgresql.org/pub/repos/apt/ wheezy-pgdg main" /etc/apt/sources.list.d/pgdg.list || echo "deb http://apt.postgresql.org/pub/repos/apt/ wheezy-pgdg main" >> /etc/apt/sources.list.d/pgdg.list
+wget https://www.postgresql.org/media/keys/ACCC4CF8.asc
+apt-key add ACCC4CF8.asc && rm ACCC4CF8.asc
+apt-get update
+apt-get install -y postgresql-$PGSQL_VERSION libpq-dev
+# cp $PROJECT_DIR/etc/install/pg_hba.conf /etc/postgresql/$PGSQL_VERSION/main/
+service postgresql reload
 
 # virtualenv global setup
-if ! command -v pip; then
-    easy_install -U pip
-fi
 if [[ ! -f /usr/local/bin/virtualenv ]]; then
     pip install virtualenv virtualenvwrapper stevedore virtualenv-clone
 fi
@@ -56,42 +57,28 @@ fi
 cp -p $PROJECT_DIR/etc/install/bashrc /home/vagrant/.bashrc
 su - vagrant -c "mkdir -p /home/vagrant/.pip_download_cache"
 
-# Node.js, CoffeeScript and LESS
-if ! command -v npm; then
-    wget http://nodejs.org/dist/v0.10.0/node-v0.10.0.tar.gz
-    tar xzf node-v0.10.0.tar.gz
-    cd node-v0.10.0/
-    ./configure && make && make install
-    cd ..
-    rm -rf node-v0.10.0/ node-v0.10.0.tar.gz
-fi
-if ! command -v coffee; then
-    npm install -g coffee-script
-fi
-if ! command -v lessc; then
-    npm install -g less
-fi
-
 # ---
 
 # postgresql setup for project
-createdb -Upostgres $DB_NAME
+createdb -U postgres "$DB_NAME"_dev
 
 # virtualenv setup for project
 su - vagrant -c "/usr/local/bin/virtualenv $VIRTUALENV_DIR && \
     echo $PROJECT_DIR > $VIRTUALENV_DIR/.project && \
     PIP_DOWNLOAD_CACHE=/home/vagrant/.pip_download_cache $VIRTUALENV_DIR/bin/pip install -r $PROJECT_DIR/requirements.txt"
 
-echo "workon $VIRTUALENV_NAME" >> /home/vagrant/.bashrc
+grep -q -F "SECRET_KEY" $VIRTUALENV_DIR/bin/activate ||
+    echo export SECRET_KEY=\"`</dev/urandom tr -dc 'abcdefghijklmnopqrstuvwxyz0123456789\!@#$%^\&*\(-_=+\)' | head -c50; echo ""`\" \
+        >> $VIRTUALENV_DIR/bin/activate
+
+grep -q -F "DJANGO_SETTINGS_MODULE" $VIRTUALENV_DIR/bin/activate ||
+    echo export DJANGO_SETTINGS_MODULE=$PROJECT_NAME.settings.local \
+        >> $VIRTUALENV_DIR/bin/activate
+
+grep -q -F "workon $VIRTUALENV_NAME" /home/vagrant/.bashrc || echo "workon $VIRTUALENV_NAME" >> /home/vagrant/.bashrc
 
 # Set execute permissions on manage.py, as they get lost if we build from a zip file
 chmod a+x $PROJECT_DIR/manage.py
 
 # Django project setup
 su - vagrant -c "source $VIRTUALENV_DIR/bin/activate && cd $PROJECT_DIR && ./manage.py syncdb --noinput && ./manage.py migrate"
-
-# Add settings/local.py to gitignore
-if ! grep -Fqx $LOCAL_SETTINGS_PATH $PROJECT_DIR/.gitignore
-then
-    echo $LOCAL_SETTINGS_PATH >> $PROJECT_DIR/.gitignore
-fi
